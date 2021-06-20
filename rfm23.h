d3d15068bc22002f43fb378fc29a521fe86c5669
TonyLoreto <59941360+TonyLoreto@users.noreply.github.com>
2021-06-20 17:17:51 -0500

    20-06-21

--- rfm23.h
+++ rfm23.h
@@ -26,21 +26,30 @@
 uint8_t tempVal = 0;
 
 //Configuracion de Frecuencia
-#define uint8_t freqBand= 19;
+#define uint8_t freqBandSelect= 0x75;//Frecuency Band Sel Reg
+#define uint8_t nominalCarrierF1= 0x76;//
+#define uint8_t nominalCarrierF0= 0x77;
+#define uint8_t fB= 19;
 
 #define uint16_t frqTx= 50240;
 #define uint16_t frqRx= 50400;
 
 //Salto de frecuencia
+#define uint8_t frqHopCh= 0x79;//Reg
+#define uint8_t frqHopStep= 0x7A;//Reg
 #define uint8_t hopToTx= 0;
 #define uint8_t hopToRx= 1;
 #define uint8_t stepSize= 2;
 
 //Desviacion de Frecuencia
+#define uint8_t modMC= 0x71;//Reg Modulation Mode Control
+#define uint8_t freqDev= 0x72;//Rrg frequency Deviation
 #define uint16_t desvON= 0;//Por definir
 #define uint16_t desvOFF=0;
 
 //Desplazamiento de Frecuencia
+#define uint8_t freqOffset0= 0x73;//Reg Frequency Offset 0
+#define uint8_t freqOffset1= 0x74;//Ref Frequency Offset 1
 uint16_t offset= 0;
 
 //Tasa de Datos
@@ -62,7 +71,6 @@ bool lna= 0;
 //Modos de operacion
 void setOpMode(uint8_t mode){
 	uint8_t aux = mode;
-		setFrqBndSel(freq, hesel)
 	if( aux == standby )
 	{
 		write(regOpMode, standby);
@@ -70,10 +78,12 @@ void setOpMode(uint8_t mode){
 	if( aux == transmit )
 	{
 		write(regOpMode, transmit);
+		setFrqBndSel(FrqTx, 1);
 	}
 	if( aux == receive )
 	{
 		write(regOpMode, receive);
+		setFrqBndSel(FrqRx, 1);
 	}
 	if( aux == sensor )
 	{
@@ -90,31 +100,53 @@ void setOpMode(uint8_t mode){
 }
 
 //Configuracion de Frecuencia
-void setFrqBndSel(uint8_t freqBand, bool hbsel){
-
+//hbsel depende de 
+void setFrqBndSel(uint16_t freq, bool hbsel){
+	uint8_t aux = 0;
+	if (hbsel=1)
+	{
+		aux = read(freqBandSelect) | fB | (hbsel<<5);
+		write(freqBandSelect, aux);
+		write(nominalCarrierF1, (uint8_t)(freq >> 8));
+		write(nominalCarrierF0, (uint8_t)(freq));
+	}
+	if (hbsel=0)
+		{
+			aux = read(freqBandSelect) | fB & (hbsel<<5);
+			write(freqBandSelect, aux);
+			write(nominalCarrierF1, (uint8_t)(freq >> 8));
+			write(nominalCarrierF0, (uint8_t)(freq));
+		}
 }
 
 //Establecimiento de Frecuencia
-void setFrq(uint16_t freq){
+//void setFrq(uint16_t freq){
+//}
 
-}
-//Selector de Canal
-void setFrqHop(uint8_t hop){//Sugiero cambiar hop a chanel
-	
+//Selector de Frecuencia de Salto 
+//freq+hop*(stepSize*10KHz)
+void setFrqHop(uint16_t freq, bool hsel, uint8_t hop, uint8_t stepSize){
+	setFrqBndSel(freq, hesel);
+	setStpHop(hop, stepSize);
 }
 //Salto de Frecuencia
-void setStpHop(uint8_t stepSize){
-
+void setStpHop(uint8_t hop, uint8_t stepSize){//hop*(stepSize*10KHz)
+	write(frqHopCh, hop);
+	write(frqHopStep, stepSize);
 }
 
 //Desviacion de Frecuencia
 void setFrqDev(uint16_t desvFreq){
-
+	uint8_t aux= 0;
+	aux = read(modMC) | desvFreq & (1<<8); 
+	write(modMC, aux);
+	write(freqDev, (uint8_t)(desvFreq));
 }
 
 //Desplazamiento de Frecuencia
 void setFrqOff(uint16_t numOffset){
-
+	write(freqOffset0, (uint8_t)(numOffset));
+	write(freqOffset1, numOffset & (2<<8));
 }
 
 //Tasa de Datos
@@ -131,8 +163,10 @@ void setTxKbps(uint16_t numDataRate, bool rang){
 }
 
 //Potencia
-void setTxPwr(bool lna_sw, uint8_t){
-
+void setTxPwr(bool lna_sw, uint8_t txPow){
+	uint8_t aux = 0;
+	aux = read(outPower) | txPow | (lna_sw<<3);
+	write(outPower, aux);
 }
 
 //Modulacion
